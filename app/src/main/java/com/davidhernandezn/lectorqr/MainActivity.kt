53984b78a7872434e97ddb5912e1d7034a5d55b6
Davidhernandezn <d4vidhernandezn@gmail.com>
2021-12-26 16:05:12 -0600

    implementación del codigo qr con obtención de la información mostrado en texto, su manejo es en horizontal

--- app/src/main/java/com/davidhernandezn/lectorqr/MainActivity.kt
+++ app/src/main/java/com/davidhernandezn/lectorqr/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.davidhernandezn.lectorqr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.davidhernandezn.lectorqr.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnEscanear.setOnClickListener { initEscanear()}
+    }
+
+    private fun initEscanear() {
+        IntentIntegrator(this).initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if(result != null){
+            if (result.contents == null){
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Valor escaneado: ${result.contents}", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
