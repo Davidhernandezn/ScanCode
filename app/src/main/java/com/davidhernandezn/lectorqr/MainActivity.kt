e9c14bfd33855a1e3efcb1a134a4bcc7f2d48e8d
Davidhernandezn <d4vidhernandezn@gmail.com>
2021-12-27 09:35:34 -0600

    Se arreglaron errores de lectura de codigo, ahora podemos visitar mediante webview el enlace que nos envie el codigo

--- app/src/main/java/com/davidhernandezn/lectorqr/MainActivity.kt
+++ app/src/main/java/com/davidhernandezn/lectorqr/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.davidhernandezn.lectorqr
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.davidhernandezn.lectorqr.databinding.ActivityMainBinding
 import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnEscanear.setOnClickListener { initEscanear()}
+
+
     }
 
     private fun initEscanear() {
@@ -28,12 +38,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
         if(result != null){
             if (result.contents == null){
                 Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
             }else{
-                Toast.makeText(this, "Valor escaneado: ${result.contents}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Buscando... ${result.contents}", Toast.LENGTH_SHORT).show()
+                //var url = {result.contents}
+                var mywebview = findViewById<WebView>(R.id.webview)
+                mywebview.webChromeClient = object : WebChromeClient(){
+
+                }
+
+                mywebview.webViewClient = object : WebViewClient() {
+
+                }
+                var settings = mywebview.settings
+                settings.javaScriptEnabled = true
+                mywebview.loadUrl("${result.contents}")
             }
+
         }else{
             super.onActivityResult(requestCode, resultCode, data)
         }
